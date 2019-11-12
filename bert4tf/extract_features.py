9efd0c2891e95d48bbb96f7b8b751e1f4351d30a
Xu <xushengquan@souche.com>
2019-11-12 18:34:53 +0800

    特征提取

--- bert4tf/extract_features.py
+++ bert4tf/extract_features.py
@@ -264,6 +264,113 @@ def predict_model_builder(bert_config, init_checkpoint, num_labels, use_one_hot_
 
     return model_fn
 
+def convert_lst_to_features(lst_str, seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    for (ex_index, example) in enumerate(read_sentences_examples(lst_str)):
+        tokens_a = tokenizer.tokenize(example.text_a)
+
+        tokens_b = None
+        if example.text_b:
+            tokens_b = tokenizer.tokenize(example.text_b)
+
+        if tokens_b:
+            # Modifies `tokens_a` and `tokens_b` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            _truncate_seq_pair(tokens_a, tokens_b, seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(tokens_a) > seq_length - 2:
+                tokens_a = tokens_a[0:(seq_length - 2)]
+
+        # The convention in BERT is:
+        # (a) For sequence pairs:
+        #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+        #  type_ids: 0     0  0    0    0     0       0 0     1  1  1  1   1 1
+        # (b) For single sequences:
+        #  tokens:   [CLS] the dog is hairy . [SEP]
+        #  type_ids: 0     0   0   0  0     0 0
+        #
+        # Where "type_ids" are used to indicate whether this is the first
+        # sequence or the second sequence. The embedding vectors for `type=0` and
+        # `type=1` were learned during pre-training and are added to the wordpiece
+        # embedding vector (and position vector). This is not *strictly* necessary
+        # since the [SEP] token unambiguously separates the sequences, but it makes
+        # it easier for the model to learn the concept of sequences.
+        #
+        # For classification tasks, the first vector (corresponding to [CLS]) is
+        # used as as the "sentence vector". Note that this only makes sense because
+        # the entire model is fine-tuned.
+        tokens = []
+        input_type_ids = []
+        tokens.append("[CLS]")
+        input_type_ids.append(0)
+        for token in tokens_a:
+            tokens.append(token)
+            input_type_ids.append(0)
+        tokens.append("[SEP]")
+        input_type_ids.append(0)
+
+        if tokens_b:
+            for token in tokens_b:
+                tokens.append(token)
+                input_type_ids.append(1)
+            tokens.append("[SEP]")
+            input_type_ids.append(1)
+
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+        # The mask has 1 for real tokens and 0 for padding tokens. Only real
+        # tokens are attended to.
+        input_mask = [1] * len(input_ids)
+
+        # Zero-pad up to the sequence length.
+        while len(input_ids) < seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            input_type_ids.append(0)
+
+        assert len(input_ids) == seq_length
+        assert len(input_mask) == seq_length
+        assert len(input_type_ids) == seq_length
+
+        # if ex_index < 5:
+        #     tf.logging.info("*** Example ***")
+        #     tf.logging.info("unique_id: %s" % (example.unique_id))
+        #     tf.logging.info("tokens: %s" % " ".join(
+        #         [tokenization.printable_text(x) for x in tokens]))
+        #     tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+        #     tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+        #     tf.logging.info(
+        #         "input_type_ids: %s" % " ".join([str(x) for x in input_type_ids]))
+
+        yield InputFeatures(
+            unique_id=example.unique_id,
+            tokens=tokens,
+            input_ids=input_ids,
+            input_mask=input_mask,
+            input_type_ids=input_type_ids)
+
+def read_sentences_examples(lst_strs):
+    """Read a list of `InputExample`s from a list of strings."""
+    unique_id = 0
+    for ss in lst_strs:
+        line = tokenization.convert_to_unicode(ss)
+        if not line:
+            continue
+        line = line.strip()
+        text_a = None
+        text_b = None
+        m = re.match(r"^(.*) \|\|\| (.*)$", line)
+        if m is None:
+            text_a = line
+        else:
+            text_a = m.group(1)
+            text_b = m.group(2)
+        yield InputExample(unique_id=unique_id, text_a=text_a, text_b=text_b)
+        unique_id += 1
+
 def model_fn_builder(bert_config, init_checkpoint, layer_indexes, use_tpu,
                      use_one_hot_embeddings):
     """Returns `model_fn` closure for TPUEstimator."""
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name="bert4tf",
-    version="1.0.0",
+    version="1.0.1",
     keywords=("bert4tf", "tensorflow"),
     description="bert for tensorflow",
     long_description="...",
@@ -29,3 +29,4 @@ setup(
             'Programming Language :: Python :: 3.6'
         ]
 )
+print("Welcome to Bert4TF")
